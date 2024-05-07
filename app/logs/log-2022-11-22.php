4c2accb2a1dbc879221e9ee17766c4d0cd2376c1
Handoyo Saputra <handoyosaputra4@gmail.com>
2024-05-07 17:24:26 +0700

    clear cached

--- app/logs/log-2022-11-22.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2022-11-22 10:36:55 --> Could not find the language line "pole_display"
-ERROR - 2022-11-22 10:37:14 --> Could not find the language line "pole_display"
--- files/db_updates/3.4.53.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `sma_settings` ADD COLUMN `ksa_qrcode` tinyint(1) NULL;
-UPDATE `sma_settings` SET `version` = '3.4.53' WHERE `setting_id` = 1;
